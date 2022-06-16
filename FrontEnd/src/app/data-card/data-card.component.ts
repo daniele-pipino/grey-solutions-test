8027c7d97d86b07e6944eb4c6b25146d986b3d73
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-06-16 12:46:39 +0200

    product list finished

--- FrontEnd/src/app/data-card/data-card.component.ts
+++ FrontEnd/src/app/data-card/data-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Data } from 'src/model/data.modul';
 
 @Component({
   selector: 'app-data-card',
   templateUrl: './data-card.component.html',
-  styleUrls: ['./data-card.component.css']
+  styleUrls: ['./data-card.component.css'],
 })
 export class DataCardComponent implements OnInit {
+  @Input()
+  item!: Data;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
