f09a84e1eee4d7153df972b304c2b898fbc3ffa6
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-06-16 10:14:54 +0200

    SpringBoot project

---
+++ BackEnd/src/main/java/com/greySolutions/prove/ProveApplication.java
@@ -0,0 +1,13 @@
+package com.greySolutions.prove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveApplication.class, args);
+	}
+
+}
