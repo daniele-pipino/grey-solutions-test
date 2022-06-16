8c4d3590bdfefb2d84607f1a31a9bb231d87fd3a
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-06-16 11:28:36 +0200

    defined data model

---
+++ FrontEnd/src/model/data.modul.ts
@@ -0,0 +1,9 @@
+export class Data {
+  constructor(
+    public id: string,
+    public code: number,
+    public label: number,
+    public device_id: string,
+    public current_job: string
+  ) {}
+}
